19fafeee0021e71747aa658b7e6fd6232090ee13
Shyheim Russell <shyheim.russell@students.olin.edu>
2018-04-02 20:56:19 -0400

    Updated game code

--- racing_game.py
+++ racing_game.py
@@ -1,19 +1,44 @@
 import pygame
 
 
-class PyGameWindowView(object):
-    
+class Window(object):
+    """A view of the Game window"""
+    def __int__(self):
+        pass
+
+
+class Map(object):
+    pass
+
+
+class Racer(object):
+    pass
 
 
 
-Main Loop:
-
-    Player Input
-
-    Update pygame
+class Controllers(object):
+    pass
 
 
-    Check for events
+class Sprites(object):
+    pass
 
 
-    print Game
+class CPU(object):
+    pass
+
+
+
+def race():
+    while running:
+        """Get Player Input"""
+
+
+        """Update the game"""
+
+
+        """Check for events"""
+
+
+
+        """print the game"""
