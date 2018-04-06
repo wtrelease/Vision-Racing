a805eece87952bb547476c2ba10e66232341f558
Shyheim Russell <shyheim.russell@students.olin.edu>
2018-04-06 15:04:36 -0400

    Updated code

--- racing_game.py
+++ racing_game.py
@@ -1,12 +1,22 @@
 import pygame
-
+import time
+import os
+import random
+import cv2
+import numpy
+import sys
 
 class Window(object):
     """A view of the Game window"""
-    def __int__(self):
+    def __int__(self, model, size):
+        self.model = model
+        self.screen = pygame.display.set_mode(size)
+
+    def draw(self):
         pass
 
-
+class Model(object):
+    """Encodes a model of the game state"""
 class Map(object):
     pass
 
@@ -30,6 +40,11 @@ class CPU(object):
 
 
 def race():
+    pygame.init()
+
+    size = (1800, 800)
+    model = Model(size)
+    view = Window(model, size)
     while running:
         """Get Player Input"""
 
@@ -42,3 +57,4 @@ def race():
 
 
         """print the game"""
+        pass
