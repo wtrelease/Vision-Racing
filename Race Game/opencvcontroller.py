bc2a880a931ba53c9bda4eed8278a49440afe5c5
Shyheim Russell <shyheim.russell@students.olin.edu>
2018-04-17 14:18:49 -0400

    Controller

--- Race Game/opencvcontroller.py
+++ Race Game/opencvcontroller.py
@@ -3,32 +3,33 @@ from collections import deque
 import cv2
 import cv2.aruco as aruco
 import os
+import glob
 
+def controller(cap):
+    cap = cv2.VideoCapture(0)
 
-cap = cv2.VideoCapture(0)
+    while True:
+        # Capture frame by frame
+        ret, frame = cap.read()
 
-while True:
-    # Capture frame by frame
-    ret, frame = cap.read()
+        #print(from shape) 480x640
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+        parameters = aruco.DetectorParameters_create()
+        #print(parameters)
 
-    #print(from shape) 480x640
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
-    parameters = aruco.DetectorParameters_create()
-    #print(parameters)
+            #Lists of ids and the corners belonging to each id
+        corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict,
+            parameters=parameters)
+        print(corners)
 
-        #Lists of ids and the corners belonging to each id
-    corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict,
-        parameters=parameters)
-    print(corners)
+        gray = aruco.drawDetectedMarkers(gray, corners)
 
-    gray = aruco.drawDetectedMarkers(gray, corners)
+        #print(rejectedImgPoints)
+        #Display the resulting frame
+        cv2.imshow('frame', gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-    #print(rejectedImgPoints)
-    #Display the resulting frame
-    cv2.imshow('frame', gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows
+    cap.release()
+    cv2.destroyAllWindows
