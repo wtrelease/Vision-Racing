50c67ea5d4224dde09fe04f5b16ec3e7dbcc3318
Shyheim Russell <shyheim.russell@students.olin.edu>
2018-04-18 16:50:24 -0400

    Updated Controller

--- Race Game/opencvcontroller.py
+++ Race Game/opencvcontroller.py
@@ -4,32 +4,35 @@ import cv2
 import cv2.aruco as aruco
 import os
 import glob
+import math
 
-def controller(cap):
-    cap = cv2.VideoCapture(0)
+def controller(cap,old_turn):
 
-    while True:
-        # Capture frame by frame
-        ret, frame = cap.read()
-
-        #print(from shape) 480x640
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
-        parameters = aruco.DetectorParameters_create()
-        #print(parameters)
-
+    #while True:
+    # Capture frame by frame
+    ret, frame = cap.read()
+    #print(from shape) 480x640
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+    parameters = aruco.DetectorParameters_create()
+    #print(parameters)
             #Lists of ids and the corners belonging to each id
-        corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict,
-            parameters=parameters)
-        print(corners)
-
-        gray = aruco.drawDetectedMarkers(gray, corners)
-
-        #print(rejectedImgPoints)
-        #Display the resulting frame
-        cv2.imshow('frame', gray)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows
+    corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict,
+        parameters=parameters)
+    #print(corners)
+    for tag in corners:
+        TL = tag[0][0]
+        BL = tag[0][3]
+        dx = TL[0]-BL[0]
+        dy = TL[1]-BL[1]
+        angle = math.atan(dx/dy)
+        return (angle*180/math.pi)/90
+    gray = aruco.drawDetectedMarkers(gray, corners)
+            #print(rejectedImgPoints)
+    #Display the resulting frame
+    #cv2.imshow('frame', gray)
+    # if cv2.waitKey(1) & 0xFF == ord('q'):
+    #     cap.release()
+    #     cv2.destroyAllWindows
+    #     break
+    return old_turn
